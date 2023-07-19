4e1135f778d2cc478498e18a9f91533183dccfbc
avish24x <mileshtaukoorah@gmail.com>
2023-07-19 17:31:30 +0400

    added video text need to review why hovering is not working on other slideshow added manifest.jscon

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const shopNowBtn = document.getElementById("shopNowBtn");
+
+  shopNowBtn.addEventListener("click", () => {
+    alert("You clicked the button!");
+    console.log("test");
+  });
+});
